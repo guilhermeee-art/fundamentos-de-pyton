6dfcb9af20e3cc6c7463d2a62b1773cbf61f9c1e
Samuel Silva <samuel61924766@edu.pa.senac.br>
2026-03-23 20:05:15 -0300

    Update ola-mundo.py

--- ola-mundo.py
+++ ola-mundo.py
@@ -1,2 +1,28 @@
 print("olá mundo")
 print("olá samuel")
+print("samuel guilhermeeeeeeeeeee")
+#variáveis
+var = "joao"
+print(var) 
+var = 15
+print(var)
+
+#tipos de dados
+#inteiro (int)
+inteiro = 1
+#fracionários (float)
+fracionários = 1.0
+#textos
+texto = "exemplo"
+
+#lógico (Boolean)
+logico = True 
+#vazio
+vazio = None
+print(2+2)
+print(2-1)
+print(5*2)
+print(10/2)
+print(5**2)
+print(10//3)
+print(10%3)
